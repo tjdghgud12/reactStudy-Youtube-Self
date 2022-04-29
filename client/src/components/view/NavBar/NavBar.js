29b0bbdba2ab5e006db9c979c6a2e7be7b783299
SeongHoHyeong <tjdghgud12@naver.com>
2022-04-29 18:20:53 +0900

    20220429paradigm

---
+++ client/src/components/view/NavBar/NavBar.js
@@ -0,0 +1,53 @@
+import React, { useState }from 'react'
+import { Navbar, NavbarBrand, NavItem, NavLink, Nav, Collapse, NavbarToggler } from 'reactstrap';
+
+
+
+function NavBar() {
+    return (
+        <div>
+            <Navbar color='dark' expand="md" light dark >
+                <NavbarBrand href="/">
+                    ReactStudy
+                </NavbarBrand>
+                <Collapse navbar>
+                    <Nav navbar className="me-auto">
+                        <NavItem>
+                            <NavLink href='/'>
+                                Home
+                            </NavLink>
+                        </NavItem>
+
+                    </Nav>
+                    <Nav navbar>
+                    <NavItem>
+                            <NavLink href='/login'>
+                                Sign In
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href='/register'>
+                                Sign Up
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+    )
+}
+
+export default NavBar
+
+
+/* 
+    reactStrap 관련 메모
+    Collapse: 펼치고 접는 기능. Show()와 hide()와 다르게 애니메이션처럼 스르륵 펼치고 접히는걸 볼 수 있다.
+               
+ */
+
+
+
+
+
+    
--- client/src/index.js
+++ client/src/index.js
@@ -12,19 +12,20 @@ import Redusers from './_reducers/reducers'
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
     <Provider 
       store={createStoreWithMiddleware(Redusers,
-        window.__REDUX_EDVTOOLS_EXTENSION__&&
-        window.__REDUX_REVTOOLS_EXTENSIN()
+        window.__REDUX_DEVTOOLS_EXTENSION__&&
+        window.__REDUX_DEVTOOLS_EXTENSIN()
       )}
     >
       <App />
     </Provider>
     
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
